680f9b89bdc03cdd57b3854a8ac5d0d53e2fa74e
CanhVeTaiGia <lvy16012005@gmail.com>
2025-11-17 14:40:55 +0700

    add

--- ss1/b8.py
+++ ss1/b8.py
@@ -1,2 +1,4 @@
+import math
+
 float_negative_number = float(input("Nhập một số thực âm: "))
-print("Số nguyên sau khi làm tròn và chuyển đổi sang số nguyên là:", int(round(float_negative_number)))
+print("Số nguyên sau khi làm tròn và chuyển đổi sang số nguyên là:", math.ceil(float_negative_number))
